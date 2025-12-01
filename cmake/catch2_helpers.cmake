f657e8421d3b33ac62cd085ca3d1ed0ac65f5cc0
amdazm05 <amdali1998@yahoo.com>
2025-12-01 21:55:47 +0000

    cmake: Catch2 remove default compilation with main
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

--- cmake/catch2_helpers.cmake
+++ cmake/catch2_helpers.cmake
@@ -1,4 +1,4 @@
 function(CompileTestFile TEST_NAME FILE_PATH)
     add_executable(${TEST_NAME} ${FILE_PATH})
-    target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2WithMain)
+    target_link_libraries(${TEST_NAME} PRIVATE Catch2::Catch2)
 endfunction(CompileTestFile FILE_PATH)

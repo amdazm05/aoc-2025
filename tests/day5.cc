3dea1ec28246c213281752727fddfa0193d88b9d
amdazm05 <amdali1998@yahoo.com>
2025-12-05 21:31:35 +0000

    ci,day5: Add day 5 implementation
    Signed-off-by: amdazm05 <amdali1998@yahoo.com>

---
+++ tests/day5.cc
@@ -0,0 +1,91 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+#include <string_utils.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+// [1,3],[2,6],[8,10]
+
+using namespace string_helpers;
+std::string global_test_input_path;
+
+TEST_CASE( "day5", "day5" ) {
+    auto inputs = get_inputs_from_file(global_test_input_path);
+    auto entries = inputs.size();
+
+    std::vector<std::pair<uint64_t,uint64_t>> ranges;
+    std::vector<uint64_t> store;
+    std::unordered_set<uint64_t> uset;
+
+    bool later_half =false;
+
+    for(auto & in: inputs) {
+        if(in.size()==0) {
+            later_half = true;
+            continue;
+        }
+        if(later_half) {
+            store.push_back(std::stoll(in));
+            continue;
+        }
+        if(!later_half) {
+            auto res =get_list_seperated_with_delim(in,'-');
+            ranges.push_back({res[0],res[1]});
+        }
+    }
+
+    int result = 0;
+    for (auto st: store) {
+        for(auto p:ranges) {
+            if(p.first <= st && p.second >= st){
+                result++;
+                break;
+            }
+        }
+    }
+    UNSCOPED_INFO("Part-1: "<<result);
+    CHECK(true);
+
+    uint64_t result_ = 0;
+    /* Sorts by the first value by default */
+    std::sort(ranges.begin(),ranges.end());
+    std::vector<std::pair<uint64_t,uint64_t>> merged;
+
+    int n = ranges.size();
+    for(int i=0;i<n;i++) {
+        auto [start,end] = ranges[i];
+        if(!merged.empty() && merged.back().second >= end)
+            continue;
+        for(int k=i+1;k<n;k++) {
+            if(ranges[k].first<=end) {
+                end = std::max(end, ranges[k].second);
+            }
+        }
+        merged.push_back({start,end});
+        result_+=end-start+1;
+    }
+
+    UNSCOPED_INFO("Part-2: "<<result_);
+    CHECK(true);
+}
+
+int main(int argc, char* argv[]) {
+    Catch::Session session;
+
+    using namespace Catch::Clara;
+
+    auto cli =
+        session.cli()
+        | Opt(global_test_input_path, "file")
+            ["--input"]
+            ("input file for test");
+
+    session.cli(cli);
+
+    int returnCode = session.applyCommandLine(argc, argv);
+    if (returnCode != 0)
+        return returnCode;
+
+    return session.run();
+}
